e3c89af11620a705a9838799fcc1eae882a62f3c
Marcus Schoch <magge.schoch@gmail.com>
2018-12-20 11:00:32 +0100

    working loop after transition

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,12 +1,23 @@
 
-document.querySelector('video').addEventListener('ended', function () {
+document.querySelector('#loop_1').addEventListener('ended', function () {
+    this.play();
+})
+document.querySelector('#loop_2').addEventListener('ended', function () {
     this.play();
 })
 
 function changeToSourceTwo() {
-    document.getElementById('loop').addEventListener('ended', myHandler, false);
+    document.getElementById('loop_1').addEventListener('ended', myHandler, false);
+    var vid = document.getElementById('transition')
     function myHandler(e) {
         document.getElementById("transition").style.display = "block";
-        document.getElementById("loop").style.display = "none";
+        document.getElementById("loop_1").style.display = "none";
+        vid.play();
     };
 }
+
+document.getElementById('transition').addEventListener('ended', myHandler, false);
+function myHandler(e) {
+    document.getElementById("transition").style.display = "none";
+    document.getElementById("loop_2").style.display = "block";
+};
