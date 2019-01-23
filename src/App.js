5745278e03f4d33c73f35b115c5d0143c3fa3105
Jan Schneider <jan.schneider@hfg-gmuend.de>
2019-01-23 20:56:53 +0100

    states into switch

--- src/App.js
+++ src/App.js
@@ -38,8 +38,9 @@ class App extends React.Component {
   state = {
     // 0 Screensaver, 1 Aufbau, 2 Daten, 3 Simulation, 4 Anwendungen
     actualState: 0,
-    lastState:-1,
+    lastState:0,
     started: false,
+    showMenu: false,
     // screensaver
     showScreensaverBasic: true,
     // Aufbau
@@ -59,19 +60,16 @@ class App extends React.Component {
 
   handleClickShowData = param => e => {
     if (param === "screensaver") {
-      this.setState({ actualState: 1, showAnwendungenBasic: false, showScreensaverBasic: true });
+      this.setState({ actualState: 0 });
       this._three.handleClickThree(0, 45, -0.01);
     } else if (param === "aufbau") {
-      this.setState({ actualState: 1, showAnwendungenBasic: false, showScreensaverBasic: false, showAufbauBasic: true });
-      console.log(this.state.showAufbauBasic);
+      this.setState({ actualState: 1 });
     } else if (param === "daten") {
-      this.setState({ actualState: 2, showAufbauBasic: false, showDatenBasic: true });
+      this.setState({ actualState: 2 });
     } else if (param === "simulation") {
-      this.setState({ actualState: 3, showDatenBasic: false, showDatenExpert1: false, showDatenExpert2: false, showDatenExpert3: false, showSimulationBasic: true });
+      this.setState({ actualState: 3 });
     } else if (param === "anwendungen") {
-      this.setState({ actualState: 4, showAufbauBasic: false, showSimulationBasic: false, showSimulationExpert1: false, showSimulationExpert2: false, showAnwendungenBasic: true });
-    } else if (param === "screensaver") {
-      this.setState({ actualState: 0, showAufbauBasic: false, showDatenBasic: false, showDatenExpert1: false, showDatenExpert2: false, showDatenExpert3: false, showSimulationBasic: false, showSimulationExpert1: false, showSimulationExpert2: false, showAnwendungenBasic: false, showScreensaverBasic: true });
+      this.setState({ actualState: 4 });
     } else {
       console.log('dont know what to do with ', param);
     }
@@ -79,10 +77,30 @@ class App extends React.Component {
       this.setState({ lastState: this.state.actualState});
       this._three.handleClickThree(0, 90, -0.01);
     }
+    switch(this.state.actualState) {
+      // screensaver
+      case 0:
+        this.setState({showMenu: true, showAnwendungenBasic: false, showScreensaverBasic: true });
+        break;
+        case 1:
+        this.setState({showAnwendungenBasic: false, showScreensaverBasic: false, showAufbauBasic: true });
+        break;
+        case 2:
+        this.setState({showAufbauBasic: false, showDatenBasic: true});
+        break;
+        case 3:
+        this.setState({showDatenBasic: false, showDatenExpert1: false, showDatenExpert2: false, showDatenExpert3: false, showSimulationBasic: true });
+        break;
+        case 4:
+        this.setState({showAufbauBasic: false, showSimulationBasic: false, showSimulationExpert1: false, showSimulationExpert2: false, showAnwendungenBasic: true });
+        break;
+      default:
+        break;
+    }
   };
 
   render() {
-    const { showScreensaverBasic, showAufbauBasic, showDatenBasic, showDatenExpert1, showDatenExpert2, showDatenExpert3, showSimulationBasic, showSimulationExpert1, showSimulationExpert2, showAnwendungenBasic } = this.state;
+    const { showMenu, showScreensaverBasic, showAufbauBasic, showDatenBasic, showDatenExpert1, showDatenExpert2, showDatenExpert3, showSimulationBasic, showSimulationExpert1, showSimulationExpert2, showAnwendungenBasic } = this.state;
     return (
       <div className="App">
         <React.Fragment>
@@ -93,16 +111,25 @@ class App extends React.Component {
               content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
             />
             <Three ref={(three) => { this._three = three; }}/>
+            {/* MENU */}
+            <Fade in={!showMenu} timeout={{ enter: fadeTimeBasic*2, exit: fadeTimeBasic }}>
+            <div className="menu">
+              <Button className='n1' disabled={showMenu} style={{ position: 'absolute', right: distanceFromSide, bottom: distanceFromSide*7-40, fontSize: '20px' }} onClick={this.handleClickShowData('screensaver')} >mehr erfahren</Button>
+            </div>
+            </Fade>
+            <Fade in={showMenu} timeout={{ enter: fadeTimeBasic*2, exit: fadeTimeBasic }}>
             <div className="menu">
               <Button className='n1' disabled={!showScreensaverBasic} style={{ position: 'absolute', right: distanceFromSide, top: distanceFromSide, fontSize: '20px' }} onClick={this.handleClickShowData('aufbau')} >Aufbau</Button>
               <Button className='n1' disabled={!showAufbauBasic} style={{ position: 'absolute', right: distanceFromSide, top: 350, fontSize: '20px' }} onClick={this.handleClickShowData('daten')} >Daten</Button>
               <Button className='n1' disabled={!showDatenBasic} style={{ position: 'absolute', right: distanceFromSide, top: 700, fontSize: '20px' }} onClick={this.handleClickShowData('simulation')} >Simulation</Button>
               <Button className='n1' disabled={!showSimulationBasic} style={{ position: 'absolute', right: distanceFromSide, bottom: distanceFromSide, fontSize: '20px' }} onClick={this.handleClickShowData('anwendungen')} >Anwendungsgebiete</Button>
             </div>
+            </Fade>
             {/* SCREENSAVER */}
             <Fade in={showScreensaverBasic} timeout={{ enter: fadeTimeBasic*2, exit: fadeTimeBasic }}>
               <div className="aufbauNormal">
-                <p className='n1' style={{ position: 'absolute', color: textColorNormal , left: distanceFromSide*7, top: distanceFromSide*20, fontSize: '20px', width: '20%', textAlign: 'left' }}>Das Konzept des digitalen Zwillings setzt drei Dinge voraus:<br/><br/>
+                <p className='n1' style={{ position: 'absolute', lineHeight: '26px', color: textColorNormal , left: distanceFromSide*7, bottom: distanceFromSide*7-40, fontSize: '20px', width: '20%', textAlign: 'left' }}>
+                  Das Konzept des digitalen Zwillings setzt drei Dinge voraus:<br/><br/>
                   Es gibt ein physisches Objekt in
                   der realen Welt, ein virtuelles Objekt in der virtuellen Welt und die Verbindung der beiden durch Daten und Informationen.</p>
               </div>
