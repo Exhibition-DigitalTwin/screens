3811c1c798770d3bbb4f1eb513866075e594e447
Jan Schneider <jan.schneider@hfg-gmuend.de>
2019-01-23 13:27:00 +0100

    sending data to catch all function

--- src/App.js
+++ src/App.js
@@ -55,8 +55,7 @@ class App extends React.Component {
             />
             <Three />
             <div className="menu">
-              <Button className='n1' style={{ position: 'absolute', right: distanceFromSide, top: distanceFromSide, fontSize: '20px' }} onClick={//this.handleClickShowData('aufbau')
-              () => { console.log('onClick'); }} >Aufbau</Button>
+              <Button className='n1' style={{ position: 'absolute', right: distanceFromSide, top: distanceFromSide, fontSize: '20px' }} onClick={this.handleClickShowData('aufbau')} >Aufbau</Button>
               <Button className='n1' style={{ position: 'absolute', right: distanceFromSide, top: 350, fontSize: '20px' }}>Daten</Button>
               <Button className='n1' style={{ position: 'absolute', right: distanceFromSide, top: 700, fontSize: '20px' }}>Simulation</Button>
               <Button className='n1' style={{ position: 'absolute', right: distanceFromSide, bottom: distanceFromSide, fontSize: '20px' }}>Anwendungsgebiete</Button>
