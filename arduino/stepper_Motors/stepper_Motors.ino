a286be3213f670b85a74b7a94d711003caa0f217
Jan Schneider <jan.schneider@hfg-gmuend.de>
2019-02-06 22:16:57 +0100

    millis in micro + LED after "data" in half opacity

--- arduino/stepper_Motors/stepper_Motors.ino
+++ arduino/stepper_Motors/stepper_Motors.ino
@@ -1,4 +1,3 @@
-
 #include <FastLED.h>
 
 // LEDs
@@ -17,7 +16,6 @@ int previousLEDrun = false;
 int currentLED = 37;
 int currentLED2 = 59;
 
-
 // stepper motors
 int incomingByte = 0;
 int cabinSteps = 0;
@@ -29,12 +27,12 @@ const int dirPinCabin = 5;
 // general
 int rotateCabin = false; // false standing still, true turning
 int currentStatusLEDs = 0; // 0 off, -1 down, 1 up
-unsigned long previousMillis = 0;
-unsigned long previousMillis2 = 0;
-unsigned long previousMillisStepRotor = 0;
-unsigned long previousMillisStepCabin = 0;
-const long interval = 100;
-const long interval500 = 1;
+unsigned long previousMicros = 0;
+unsigned long previousMicros2 = 0;
+unsigned long previousMicrosStepRotor = 0;
+unsigned long previousMicrosStepCabin = 0;
+const long interval = 100000;
+const long interval500 = 1000;
 int showLEDVorne = false;
 int showLEDHinten = false;
 int stepHighRotor = false;
@@ -42,7 +40,6 @@ int stepHighCabin = false;
 
 
 void setup() {
-  //delay(3000); // 3 second delay for recovery
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   pinMode(stepPinCabin, OUTPUT);
@@ -59,7 +56,7 @@ void setup() {
 
 void loop()
 {
-  unsigned long currentMillis = millis();
+  unsigned long currentMicros = micros();
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
@@ -69,11 +66,6 @@ void loop()
     incomingByte = 0;
     rotateCabin = true;
   }
-
-  if (incomingByte != 49) {
-
-  }
-
 
   if (incomingByte == 50) {
     incomingByte = 0;
@@ -85,11 +77,10 @@ void loop()
     showLEDHinten = true;
   }
 
-
-  if (showLEDVorne == true && currentLED <= NUM_LEDS && currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+  if (showLEDVorne == true && currentLED <= NUM_LEDS && currentMicros - previousMicros >= interval) {
+    previousMicros = currentMicros;
     leds[currentLED] = CRGB::Red;
-    leds[currentLED - 1] = CRGB::Red;
+    leds[currentLED - 1] = CRGB( 128, 0, 0);
     FastLED.show();
     //FastLED.delay(100/FRAMES_PER_SECOND);
     // clear this led for the next time around the loop
@@ -97,8 +88,7 @@ void loop()
     leds[currentLED - 1] = CRGB::Black;
     //delay(100);
     currentLED++;
-  }
-  else if (currentLED == NUM_LEDS) {
+  } else if (currentLED == NUM_LEDS) {
     showLEDVorne = false;
     leds[currentLED + 2] = CRGB::Black;
     leds[currentLED + 1] = CRGB::Black;
@@ -112,23 +102,18 @@ void loop()
     currentLED = 37;
   }
 
-  if (showLEDHinten == true && currentLED2 >= 37 && currentMillis - previousMillis2 >= interval) {
-
-    previousMillis2 = currentMillis;
+  if (showLEDHinten == true && currentLED2 >= 37 && currentMicros - previousMicros2 >= interval) {
+    previousMicros2 = currentMicros;
     leds[currentLED2] = CRGB::Red;
     leds[currentLED2 - 1] = CRGB::Red;
     FastLED.show();
-    //delay(500);
-    //FastLED.delay(100/FRAMES_PER_SECOND);
     // clear this led for the next time around the loop
     leds[currentLED2] = CRGB::Black;
     leds[currentLED2 - 1] = CRGB::Black;
-    //delay(100);
     currentLED2--;
-  }
-  else if (currentLED2 == 3) {
+  } else if (currentLED2 == 3) {
     showLEDHinten = false;
-     leds[currentLED + 2] = CRGB::Black;
+    leds[currentLED + 2] = CRGB::Black;
     leds[currentLED + 1] = CRGB::Black;
     leds[currentLED] = CRGB::Black;
     leds[currentLED - 1] = CRGB::Black;
@@ -140,45 +125,39 @@ void loop()
     currentLED2 = 59;
   }
 
-
   if (rotateCabin == true && cabinSteps <= 3000) {
-    cabinStep(currentMillis);
-    
-    rotorStep(currentMillis);
+    cabinStep(currentMicros);
+    rotorStep(currentMicros);
   } else {
-    rotorStep(currentMillis);
+    rotorStep(currentMicros);
     rotateCabin = false;
     cabinSteps = 0;
-
-}
-}
-
-
-void rotorStep(unsigned long currentMillis) {
-  if(stepHighRotor == false && currentMillis - previousMillisStepRotor >= interval500){
-  digitalWrite(stepPinRotor, HIGH);
-  previousMillisStepRotor = currentMillis;
-  stepHighRotor = true;
   }
-  else if(stepHighRotor == true && currentMillis - previousMillisStepRotor >= interval500){
-  digitalWrite(stepPinRotor, LOW);
-  previousMillisStepRotor = currentMillis;
-  stepHighRotor = false;
-  } 
 }
 
-void cabinStep(unsigned long currentMillis) {
-  
-  if(stepHighCabin == false && currentMillis - previousMillisStepCabin >= interval500){
-  digitalWrite(stepPinCabin, HIGH);
-  previousMillisStepCabin = currentMillis;
-  stepHighCabin = true;
-  cabinSteps ++;
+void rotorStep(unsigned long currentMicros) {
+  if (stepHighRotor == false && currentMicros - previousMicrosStepRotor >= interval500) {
+    digitalWrite(stepPinRotor, HIGH);
+    previousMicrosStepRotor = currentMicros;
+    stepHighRotor = true;
   }
-  if(stepHighCabin == true && currentMillis - previousMillisStepCabin >= interval500){
-  digitalWrite(stepPinCabin, LOW);
-  previousMillisStepCabin = currentMillis;
-  stepHighCabin = false;
-  } 
+  else if (stepHighRotor == true && currentMicros - previousMicrosStepRotor >= interval500) {
+    digitalWrite(stepPinRotor, LOW);
+    previousMicrosStepRotor = currentMicros;
+    stepHighRotor = false;
+  }
 }
 
+void cabinStep(unsigned long currentMicros) {
+  if (stepHighCabin == false && currentMicros - previousMicrosStepCabin >= interval500) {
+    digitalWrite(stepPinCabin, HIGH);
+    previousMicrosStepCabin = currentMicros;
+    stepHighCabin = true;
+    cabinSteps ++;
+  }
+  if (stepHighCabin == true && currentMicros - previousMicrosStepCabin >= interval500) {
+    digitalWrite(stepPinCabin, LOW);
+    previousMicrosStepCabin = currentMicros;
+    stepHighCabin = false;
+  }
+}
